7c2e670b9c573dd53f92e2d25be92b371817a21d
brandonsr <brandon2004roldan@gmail.com>
2021-04-22 17:51:09 -0600

    upload all files

---
+++ JS/app.dev.js
@@ -0,0 +1,30 @@
+const getHour = ()=>{
+    const date = new Date(); 
+    // console.log(date)
+    const time = {
+        hora: date.getHours(),
+        minute: date.getMinutes(), 
+        seconds: date.getSeconds(),
+    }
+    // console.log(time)
+
+    const now = `${time.hora} : ${time.minute} : ${time.seconds}` 
+    let reloj = document.querySelector('#reloj')
+    reloj.innerHTML = now
+
+    if(time.hora<12){
+        let $good = document.querySelector('#good')
+        $good.innerHTML= "Buenos dias 🌤"
+    }
+    else if(time.hora<18){
+        let $good = document.querySelector('#good')
+        $good.innerHTML= "Buenas tardes ☀"
+    }
+    else{
+        let $good = document.querySelector('#good')
+        $good.innerHTML= "Buenas noches 🌙"
+    }
+} 
+// getHour()
+setInterval(getHour, 1)
+
